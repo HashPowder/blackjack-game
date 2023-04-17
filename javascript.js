129a573ab04f0284324cc0bc25baae0e47679b8f
HashPowder <benbra667@gmail.com>
2023-04-17 16:10:19 +0200

    Create function to begin the game when a button is clicked

--- javascript.js
+++ javascript.js
@@ -3,10 +3,15 @@ let secondCard = 11;
 let sum = firstCard + secondCard;
 let message = ""
 
-if (sum < 21) {
-    message = "Would you like to draw another card?"
-} else if (sum === 21) {
-    message = "Wohoo! You've got blackjack!"
-} else {
-    message = "Oh no! You've gone Bust!"
+
+
+function startGame() {
+    if (sum < 21) {
+        message = "Would you like to draw another card?"
+    } else if (sum === 21) {
+        message = "Wohoo! You've got blackjack!"
+    } else {
+        message = "Oh no! You've gone Bust!"
+    }
+    console.log(message)
 }
