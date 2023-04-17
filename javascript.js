e3c5235b9849c8f72969dbdd72bc4dd03252ba5f
HashPowder <benbra667@gmail.com>
2023-04-17 15:43:09 +0200

    Store resulting values from game state check  into a new variable

--- javascript.js
+++ javascript.js
@@ -1,11 +1,12 @@
 let firstCard = 10;
 let secondCard = 11;
 let sum = firstCard + secondCard;
+let message = ""
 
 if (sum < 21) {
-    console.log("Would you like to draw another card?")
+    message = "Would you like to draw another card?"
 } else if (sum === 21) {
-    console.log("Wohoo! You've got blackjack!")
+    message = "Wohoo! You've got blackjack!"
 } else {
-    console.log("Oh no! You've gone Bust!")
+    message = "Oh no! You've gone Bust!"
 }
